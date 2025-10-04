2d3a782c054d09e3de761f43b8550e57aca82b9e
Muzammil Ahmad K <Muzammil.Ahmad.K@ibm.com>
2025-10-04 17:30:09 +0530

    graphql mutation call and validator package and error handling

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,5 +1,39 @@
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+const validator = require("validator");
+
 module.exports = {
-  hello() {
-    return { text: "Hello World!", views: 1234 };
+  createUser: async function ({ userInput }, req) {
+    let errors = [];
+    if (!validator.isEmail(userInput.email)) {
+      errors.push({ message: "E-Mail is invalid." });
+    }
+    if (
+      validator.isEmpty(userInput.password) ||
+      !validator.isLength(userInput.password, { min: 5 })
+    ) {
+      errors.push({ message: "Password too short!" });
+    }
+    if (errors.length > 0) {
+      const error = new Error("Invalid input.");
+      error.data = errors;
+      error.code = 422;
+      throw error;
+    }
+
+    const existingUser = await User.findOne({ email: userInput.email });
+    if (existingUser) {
+      const error = new Error("User exists already.");
+      error.code = 422;
+      throw error;
+    }
+    const hashedPw = await bcrypt.hash(userInput.password, 12);
+    const user = new User({
+      email: userInput.email,
+      name: userInput.name,
+      password: hashedPw,
+    });
+    const createdUser = await user.save();
+    return { ...createdUser._doc, _id: createdUser._id.toString() };
   },
 };
